ca8d2f416a2d86125ef8dfcdbefbb265caa582af
HK <68671793+Hakchi06@users.noreply.github.com>
2025-08-19 11:46:37 -0500

    Declare ht_del_hash_table in hash_table.h

--- src/hash_table.h
+++ src/hash_table.h
@@ -17,3 +17,4 @@ ht_hash_table* ht_new_table(void);
 void ht_insert(ht_hash_table* ht, const char* key, const char* value);
 char* ht_search(ht_hash_table* ht, const char* key);
 void ht_delete(ht_hash_table* ht, const char* key);
+void ht_del_hash_table(ht_hash_table* ht);
